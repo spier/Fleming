d88e9b254efb5d53e6cee871e60e543ed45fd4a8
bensgitshell <benedict.butcher@shell.com>
2024-11-21 13:22:49 +0000

    First class contribution

--- src/fleming/code_summary/4o-mini_summary.py
+++ src/fleming/code_summary/4o-mini_summary.py
@@ -1,98 +1,139 @@
-# Copyright 2024 Fleming
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Databricks notebook source
-#!pip install requests
-#!pip install openai
-
-
-
-#dbutils.library.restartPython()
-
-
-
-import os
+import logging
 import requests
-import base64
 import json
-import re
+from pyspark.sql import DataFrame, SparkSession
 
+class OpenAIClient:
+    """
+    Class to interact with the OpenAI API to generate content based on a prompt and source code.
 
+    The class contains the following methods:
 
-# input source delta table and output table in workflow parameters. Table name should have number indicating token sampling rate 
+    1. call_openai: Call the OpenAI API to generate content based on the provided prompt and source code.
+    2. save_results: Save the generated results to a specified output table.
 
-delta_table = dbutils.widgets.get("delta_table")
-output_table_name = dbutils.widgets.get("output_table")
-output_table_path = f"hive_metastore.dev_innersource.{output_table_name}"
+    Example
+    --------
+    ```python
+    from fleming.code_summary.4o-mini_summary import call_openai
+    from pyspark.sql import SparkSession
 
+    # Not required if using Databricks
+    spark = SparkSession.builder.appName("openai_client").getOrCreate()
 
-def Call_OpenAI():
-  API_KEY = dbutils.secrets.get(scope="Azure-OpenAI", key="ybdevgpt3-key-1")
-  headers = {
-      "Content-Type": "application/json",
-      "api-key": API_KEY,
-  }
-  ENDPOINT = dbutils.secrets.get(scope="Azure-OpenAI", key="4o-mini-endpoint")
-  # create list for results to be appended to
-  results = []
+    spark_input_df = "your_spark_input_df"
+    output_table_name = "your_output_table"
+    prompt = "Your prompt here"
 
-  repo_contents_df = spark.read.table(delta_table)
-  #repo_contents_df = repo_contents_df.limit(10)
-  for row in repo_contents_df.collect():
-    input_source_code = row["trimmed_concatenated_content"]
-    repo_name = row["repo_name"]
-    total_token_count = row["total_token_count"]
+    client = OpenAIClient(spark, delta_table, output_table_name, prompt)
+    client.call_openai()
+    ```
 
-    payload = {
-      "messages": [
-        {
-          "role": "user",
-          "content": [
-            {
-              "type": "text",
-              "text": f"{prompt}{input_source_code}"
-            }
-          ]
+    Parameters:
+        spark (SparkSession): Spark Session
+        input_spark_df (DataFrame): Source spark DataFrame containing the input data
+        output_table_name (str): Name of the output table to save results
+        prompt (str): Prompt to send to the OpenAI API
+
+    Attributes:
+        api_key (str): API key for OpenAI
+        endpoint (str): Endpoint for OpenAI API
+        headers (dict): Headers for the API request
+    """
+
+    spark: SparkSession
+    input_spark_df: DataFrame
+    output_table_name: str
+    prompt: str
+    api_key: str
+    endpoint: str
+    headers: dict
+
+    def __init__(self, spark: SparkSession, input_spark_df: DataFrame, output_table_name: str, prompt: str, api_key: str, endpoint: str) -> None:
+        self.spark = spark
+        self.input_spark_df = input_spark_df
+        self.output_table_name = output_table_name
+        self.prompt = prompt
+        self.api_key = api_key
+        self.endpoint = endpoint
+        self.headers = {
+            "Content-Type": "application/json",
+            "api-key": self.api_key,
         }
-      ],
-      "temperature": 0.0,
-      "top_p": 0.95 #,
-      #"max_tokens": 800
-    }
+        self.results_df = None  # Initialize an instance variable to store results
+
+    def call_openai(self, title :str, concatenated_content: str, total_token_count: str) -> None:
+        """
+        Call the OpenAI API to generate summarised content based on the provided prompt and source content.
+
+        Parameters:
+        title: str - Column name for column containing summarised text title
+        concatenated_content: str - Column name for column containing concatenated content
+        total_token_count: str - Column name for column containing total token count
+
+        Returns: results_df pyspark dataframe containing summarisation of each entry
+        """
+        
+        results = []
+        repo_contents_df = self.input_spark_df
+        repo_contents_df = repo_contents_df.limit(3)
+  
+        for row in repo_contents_df.collect():
+            input_source_code = row[concatenated_content]
+            repo_name = row[title]
+            total_token_count = row[total_token_count]
+
+            payload = {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": f"{self.prompt}{input_source_code}"
+                            }
+                        ]
+                    }
+                ],
+                "temperature": 0.0,
+                "top_p": 0.95
+            }
+
+            try:
+                response = requests.post(self.endpoint, headers=self.headers, json=payload)
+                response.raise_for_status()
+                json_dict = response.json()
+                output = json_dict["choices"][0]["message"]["content"]
+                results.append((repo_name, self.prompt, total_token_count, output))
+                print(output)
+
+            except requests.RequestException as e:
+                logging.exception("Failed to make the request.")
+                raise SystemExit(f"Failed to make the request. Error: {e}")
+
+        self.results_df = self.spark.createDataFrame(results, ["repo_name", "prompt", "repo_token_count", "virtual_readme"])
 
 
- 
-  # Send request to Open AI to generate readmes with prompt
-    try:
-        response = requests.post(ENDPOINT, headers=headers, json=payload)
-        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    def display_results(self) -> None:
+        """
+        Display the generated results.
+        
+        Returns:
+        results_df dataframe gets displayed
+        """
+        if self.results_df is not None:
+          display(self.results_df)
+        else:
+          raise ValueError("No results to display. Please call call_openai() first.")
 
-        json_string = response.text
-        json_dict = json.loads(json_string)
-        output = json_dict["choices"][0]["message"]["content"]
-        results.append((repo_name,prompt,total_token_count,output))
-        print(output)
+    def save_results(self) -> None:
+        """
+        Save the generated results to the specified output table.
 
-    except requests.RequestException as e:
-        raise SystemExit(f"Failed to make the request. Error: {e}")
-
-    results_df = spark.createDataFrame(results,["repo_name","prompt","repo_token_count","virtual_readme"])
-    results_df.write.mode("overwrite").option("mergeSchema", "true").saveAsTable(f"{output_table_path}")
-
-
-    
-
-# COMMAND ----------
-
-Call_OpenAI()
+        Returns:
+        None
+        """
+        if self.results_df is not None:
+            self.results_df.write.mode("overwrite").option("mergeSchema", "true").saveAsTable(f"hive_metastore.dev_innersource.{self.output_table_name}")
+        else:
+            raise ValueError("No results to save. Please call call_openai() first.")
